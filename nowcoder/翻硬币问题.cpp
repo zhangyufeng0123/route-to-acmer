33f015fe4f337cded7d4e36699b4760f3c41fc88
zhangyufeng0123 <40848227+zhangyufeng0123@users.noreply.github.com>
2019-03-02 15:59:41 +0800

    Add files via upload

---
+++ nowcoder/翻硬币问题.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(void){
+    ios_base::sync_with_stdio(false);
+ 
+    int T;
+    cin >> T;
+    while(T--){
+        int n,m;
+        cin >> m >> n;
+        if(m!=n){
+            cout << "No" << endl;
+        }else{
+            cout << "Yes" << endl;
+        }
+    }
+}
